732dd1c379a8572c088c8e1bfab57ba40965ac96
Sanjay S <sanju15793@gmail.com>
2019-05-09 17:22:22 +0530

    added second comment

--- src/HelloWorldPGM.java
+++ src/HelloWorldPGM.java
@@ -3,5 +3,6 @@ public class HelloWorldPGM {
     public static void main(String[] args) {
         System.out.println("Hello World");
         //adding the comment
+        //remove comments
     }
 }
